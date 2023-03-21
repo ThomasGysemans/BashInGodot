a93cf40c80777b6dda4b53f0401e9c2ed93b0644
ThomasGysemans <support@sciencesky.fr>
2023-03-21 11:27:39 +0100

    Fix web export for release

--- exports/Terminal.service.worker.js
+++ exports/Terminal.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679393926|4383854115";
+const CACHE_VERSION = "1679394270|4728592183";
 const CACHE_PREFIX = "Terminal-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Terminal.offline.html";
